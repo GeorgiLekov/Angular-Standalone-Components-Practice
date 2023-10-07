8241393012c8afc3da9911bc06eaebe37de29f55
georgi.lekov <georgi.lekov@scalefocus.com>
2023-10-07 14:50:34 +0300

    Convert directive to standalone.

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
-import {SharedModule} from "../../shared/shared.module";
+import {HighlightDirective} from "../../shared/highlight.directive";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
   standalone: true,
-  imports: [SharedModule],
+  imports: [HighlightDirective],
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
