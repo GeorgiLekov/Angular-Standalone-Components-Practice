900d851dbd7e1438d53c6f28c49a1c5900da80ff
georgi.lekov <georgi.lekov@scalefocus.com>
2023-10-07 14:41:35 +0300

    Add standalone component.

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
+import {SharedModule} from "../../shared/shared.module";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
+  standalone: true,
+  imports: [SharedModule],
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
